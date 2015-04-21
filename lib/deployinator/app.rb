dd888a17c708e67ed97ce97cfbd92173f5814e4a
David Giffin <dgiffin@truecar.com>
2015-04-20 19:56:58 -0700

    Cleaned up, removed eval, etc

--- lib/deployinator/app.rb
+++ lib/deployinator/app.rb
@@ -84,20 +84,12 @@ module Deployinator
 
     get '/:stack/versions' do
       @stack = params[:stack]
-      #register_plugins(@stack) ## don't think we need this? just copied it from /:thing
-      require "views/#{@stack}"
       content_type "application/json"
-      eval("Deployinator::Views::#{@stack.classify}.new").send("#{@stack}_environments").map do |env|
+      view = Object.const_get("Deployinator::Views::#{@stack.classify}").new
+      view.send("#{@stack}_environments").map do |env|
         [ env[:name], env[:current_version].call ]
       end.to_json
-
-      ### this is the code from the old gem
-      #inst = mustache_class(@stack, settings.mustache).new
-
-      #meth = "#{@stack}_%s_#{type}"
-      #inst.push_order.collect {|env| [env, inst.send(meth % env)]}.to_json
     end
-
 
     get '/diff/:stack/:r1/:r2/?' do
       @stack = params[:stack]
